61dd226d3d4e6397e03576b885401f6d818530e6
samaelmarjan <samaaelmarjan@gmail.com>
2024-05-20 14:14:07 +0600

    config index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const dbConnection = require("./config/databaseConfig");
 const dotenv = require("dotenv").config();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const helmet = require("helmet"); //security pkg
 
 const app = express();
+
+//middlewares
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 const port = process.env.PORT || 8080;
 
